68452d03e6da5df40e8381e575debda568b4f4b1
wonderfulcho <wonderfulcho95@naver.com>
2023-08-18 12:51:07 +0900

    부산테스트 삭제 / 약국 데이터로 수정 / 보이는 화면 좌표의 값만 마커 생성하게 코드 구조 수정

--- src/main/java/com/ap4j/bma/controller/api/ApiAptController.java
+++ src/main/java/com/ap4j/bma/controller/api/ApiAptController.java
@@ -1,6 +1,6 @@
-package com.ap4j.bma.controller.busan;
+package com.ap4j.bma.controller.api;
 
-import com.ap4j.bma.model.entity.busan.BusanDTO;
+import com.ap4j.bma.model.entity.apt.AptDTO;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
@@ -16,19 +16,20 @@ import java.net.URL;
 import java.util.ArrayList;
 
 @Controller
-public class BusanController {
+public class ApiAptController {
 
-    @GetMapping("busan")
-    public String busan(Model model) {
+    /** 아파트 실거래가 api 불러오기 */
+    @GetMapping("csy")
+    public String apt(Model model) {
         StringBuffer result = new StringBuffer();
         int pageNo = 1;
 
-        ArrayList<BusanDTO> busanList = null;
+        ArrayList<AptDTO> aptList = null;
         try {
-            String apiUrl = "http://apis.data.go.kr/6260000/FoodService/getFoodKr?"
-                    + "serviceKey=5C%2FnyAagqz6%2F%2BnYRGcZyRNpteaEeTlrNaMf1KtU0CWaSMRID13wEXSHVJ0J7WMvTl864DTzD3rwHM5GPX1aWtA%3D%3D"
-                    + "&numOfRows=300"
-                    + "&pageNo=" + pageNo;
+            String apiUrl = "http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyFullDown?"
+                        + "serviceKey=5C%2FnyAagqz6%2F%2BnYRGcZyRNpteaEeTlrNaMf1KtU0CWaSMRID13wEXSHVJ0J7WMvTl864DTzD3rwHM5GPX1aWtA%3D%3D&"
+                        + "pageNo=1&"
+                        + "numOfRows=5000";
 
             URL url = new URL(apiUrl);
 
@@ -49,34 +50,34 @@ public class BusanController {
             JSONObject jsonObject = XML.toJSONObject(result.toString());
 
             // 필요한 데이터에 접근
-            JSONObject busan = jsonObject.getJSONObject("response").getJSONObject("body").getJSONObject("items");
+            JSONObject aptJson = jsonObject.getJSONObject("response").getJSONObject("body").getJSONObject("items");
 
-            busanList = new ArrayList<>();
+            JSONArray jsonArray = aptJson.getJSONArray("item");
 
-            // json을 ArrayList로 변환
-            JSONArray jsonArray = busan.getJSONArray("item");
+            aptList = new ArrayList<AptDTO>();
 
             for (int i = 0; i < jsonArray.length(); i++) {
-
                 JSONObject obj = jsonArray.getJSONObject(i);
-                String title = obj.getString("MAIN_TITLE");
-                double lng = obj.getDouble("LNG");
-                double lat = obj.getDouble("LAT");
 
-                BusanDTO busanDTO = new BusanDTO();
-                busanDTO.setMAIN_TITLE(title);
-                busanDTO.setLNG(lng);
-                busanDTO.setLAT(lat);
+                String aptName = obj.getString("dutyName");
+                String aptAddress = obj.getString("dutyAddr");
+                String aptLat = obj.optString("wgs84Lat");
+                String aptLng = obj.optString("wgs84Lon");
 
-                busanList.add(busanDTO);
+
+                AptDTO apt = new AptDTO(aptName, aptAddress, aptLng, aptLat);
+
+                aptList.add(apt);
             }
+            
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        model.addAttribute("busanList", busanList);
+        model.addAttribute("aptList", aptList);
 
-        return "kakaoMap/busan";
+        return "/kakaoMap/aptMain";
     }
+
 }
