ba99988d0e1dbadc7998a8c4a6c834b829c21e9f
우진 <leewoojin982@gmail.com>
2023-08-18 17:25:03 +0900

    layout 경로 바뀜 에러 수정

--- src/main/java/com/ap4j/bma/controller/aptTest/AptController.java
+++ src/main/java/com/ap4j/bma/controller/aptTest/AptController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -37,7 +38,7 @@ public class AptController {
 //        log.info(uniquePositions.toString());
 //        return "kakaoMap/ttt";
 //    }
-    @GetMapping("/aa")
+    @RequestMapping("aa")
     public String test(){
         return "aa";
     }
