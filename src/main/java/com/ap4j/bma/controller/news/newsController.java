e2a002358bcef06d80fc03aa403f7e9cd42ef66f
DESKTOP-0P943RL\admin <cnddh1997@hs.ac.kr>
2023-08-22 00:05:36 +0900

    뉴스테스트

--- src/main/java/com/ap4j/bma/controller/news/newsController.java
+++ src/main/java/com/ap4j/bma/controller/news/newsController.java
@@ -1,16 +1,43 @@
 package com.ap4j.bma.controller.news;
 
+import com.ap4j.bma.service.news.NewsApiService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+//@Controller
+//@CrossOrigin("*")
+//public class newsController {
+//
+//@RequestMapping("news")
+//public String newsPage(){
+//
+//    return "news/newsPage";
+//}
+//}
+@Slf4j
 @Controller
-@CrossOrigin("*")
 public class newsController {
 
-@RequestMapping("news")
+    private final NewsApiService newsApiService;
+
+    public newsController(NewsApiService newsApiService) {
+        this.newsApiService = newsApiService;
+    }
+
+    @RequestMapping("news")
 public String newsPage(){
 
     return "news/newsPage";
 }
+
+    @RequestMapping("/news/newsPage")
+    public String search(@RequestParam("query") String searchQuery, Model model) {
+        String searchResult = newsApiService.searchBlog(searchQuery);
+        model.addAttribute("searchResult", searchResult);
+        log.info(searchResult);
+        return "news/newsPage";
+    }
 }
