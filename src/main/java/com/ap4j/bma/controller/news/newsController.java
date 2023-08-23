cde0e54419663598ed80067c3397ff165e7879ba
권충오 <99638706+kwonchungoh@users.noreply.github.com>
2023-08-23 13:49:54 +0900

    테스트중

--- src/main/java/com/ap4j/bma/controller/news/newsController.java
+++ src/main/java/com/ap4j/bma/controller/news/newsController.java
@@ -31,16 +31,12 @@ public class newsController {
 
         return "news/newsPage";
     }
-//
-//// 페이지네이션 작업중
+
 //    @RequestMapping("news/newsPage")
 //    public String search(
 //            @RequestParam("query") String searchQuery,
 //            @RequestParam(value = "page", defaultValue = "1") int page,
-//            Model model
-//    ) {
-//        int resultsPerPage = 10; // 원하는 페이지당 결과 수
-//
+//            Model model) {
 //        String searchResult = newsApiService.searchNews(searchQuery);
 //
 //        ObjectMapper objectMapper = new ObjectMapper();
@@ -59,61 +55,91 @@ public class newsController {
 //                newsItem.put("link", link);
 //                newsItem.put("description", removeHTMLTags(description));
 //                newsList.add(newsItem);
+//
 //            }
 //
-//            int totalResults = newsList.size();
-//            int totalPages = (int) Math.ceil((double) totalResults / resultsPerPage);
+//            int totalResults = jsonNode.get("total").asInt(); // 뉴스의 전체 개수
 //
-//            int startIndex = (page - 1) * resultsPerPage;
-//            int endIndex = Math.min(startIndex + resultsPerPage, totalResults);
+//            int itemsPerPage = 10; // 페이지당 뉴스 항목 수
+//            int startIndex = (page - 1) * itemsPerPage;
+//
+//
+//            int endIndex = Math.min(startIndex + itemsPerPage, newsList.size());
+//            log.info(String.valueOf(endIndex)); // 10
 //
 //            List<Map<String, String>> paginatedNewsList = newsList.subList(startIndex, endIndex);
 //
 //            model.addAttribute("newsList", paginatedNewsList);
+//
+//
+//            int totalPages = (int) Math.ceil((double) totalResults / itemsPerPage);
+//
+//            log.info(String.valueOf(totalPages)); // 뉴스의 전체 개수 / 페이지당 보여줄 뉴스 항목 수
 //            model.addAttribute("currentPage", page);
 //            model.addAttribute("totalPages", totalPages);
+//            model.addAttribute("searchQuery", searchQuery);
 //
 //        } catch (IOException e) {
 //            e.printStackTrace();
-//            // Handle exception if necessary
+//            // 예외 처리를 필요에 따라 추가하세요.
 //        }
 //
 //        log.info(searchResult);
 //        return "news/newsPage";
 //    }
+@RequestMapping("news/newsPage")
+public String search(
+        @RequestParam("query") String searchQuery,
+        @RequestParam(value = "page", defaultValue = "1") int page,
+        @RequestParam(value = "start", defaultValue = "1") int start,
+        Model model) {
+    String searchResult = newsApiService.searchNews(searchQuery, start);
 
+    ObjectMapper objectMapper = new ObjectMapper();
+    try {
+        JsonNode jsonNode = objectMapper.readTree(searchResult);
+        JsonNode items = jsonNode.get("items");
 
-    @RequestMapping("/news/newsPage")
-    public String search(@RequestParam("query") String searchQuery, Model model) {
-        String searchResult = newsApiService.searchNews(searchQuery);
+        List<Map<String, String>> newsList = new ArrayList<>();
+        for (JsonNode item : items) {
+            String title = item.get("title").asText();
+            String link = item.get("link").asText();
+            String description = item.get("description").asText();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            JsonNode jsonNode = objectMapper.readTree(searchResult);
-            JsonNode items = jsonNode.get("items");
+            Map<String, String> newsItem = new HashMap<>();
+            newsItem.put("title", removeHTMLTags(title));
+            newsItem.put("link", link);
+            newsItem.put("description", removeHTMLTags(description));
+            newsList.add(newsItem);
 
-            List<Map<String, String>> newsList = new ArrayList<>();
-            for (JsonNode item : items) {
-                String title = item.get("title").asText();
-                String link = item.get("link").asText();
-                String description = item.get("description").asText();
-
-                Map<String, String> newsItem = new HashMap<>();
-                newsItem.put("title", removeHTMLTags(title));
-                newsItem.put("link", link);
-                newsItem.put("description", removeHTMLTags(description));
-                newsList.add(newsItem);
-            }
-
-            model.addAttribute("newsList", newsList);
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Handle exception if necessary
         }
 
-        log.info(searchResult);
-        return "news/newsPage";
+        int totalResults = jsonNode.get("total").asInt(); // 뉴스의 전체 개수
+
+        int itemsPerPage = 10; // 페이지당 뉴스 항목 수
+        int startIndex = (page - 1) * itemsPerPage;
+
+        int newStartIndex = startIndex + 1; // start 값을 조정
+
+        List<Map<String, String>> paginatedNewsList = newsList.subList(0, 10);
+
+        model.addAttribute("newsList", paginatedNewsList);
+
+        int totalPages = (int) Math.ceil((double) totalResults / itemsPerPage);
+
+        log.info(String.valueOf(totalPages));
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("searchQuery", searchQuery);
+
+        model.addAttribute("start", newStartIndex);
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+
+    log.info(searchResult);
+    return "news/newsPage";
+}
     public static String removeHTMLTags(String input) {
 
         String cleanText = input
