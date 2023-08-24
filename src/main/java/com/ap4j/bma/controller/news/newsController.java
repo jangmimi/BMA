23ad1b17f15e11bfdc2f56c9f941993b6b76bfc3
권충오 <99638706+kwonchungoh@users.noreply.github.com>
2023-08-24 17:42:04 +0900

    css 해야됌

--- src/main/java/com/ap4j/bma/controller/news/newsController.java
+++ src/main/java/com/ap4j/bma/controller/news/newsController.java
@@ -30,7 +30,7 @@ public class newsController {
     public String newsPage(Model model) {
         String defaultSearchQuery = "부동산"; // 기본 검색어 설정
 
-        return search(defaultSearchQuery, 1, 1, model); // 기본 검색어와 함께 search 메서드 호출
+        return search(defaultSearchQuery, 1, 1, model); // 처음 페이지에 기본 검색어를 이용하여 결과 출력
     }
 
 
@@ -46,32 +46,43 @@ public String search(
     try {
         JsonNode jsonNode = objectMapper.readTree(searchResult);
         JsonNode items = jsonNode.get("items");
+
         log.info(String.valueOf(items));
         List<Map<String, String>> newsList = new ArrayList<>();
-        for (JsonNode item : items) {
-            String title = item.get("title").asText();
-            String link = item.get("link").asText();
-            String description = item.get("description").asText();
+        if (items != null) {
+            for (JsonNode item : items) {
+                String title = item.get("title").asText();
+                String link = item.get("link").asText();
+                String description = item.get("description").asText();
 
-            Map<String, String> newsItem = new HashMap<>();
-            newsItem.put("title", removeHTMLTags(title));
-            newsItem.put("link", link);
-            newsItem.put("description", removeHTMLTags(description));
-            newsList.add(newsItem);
-
+                Map<String, String> newsItem = new HashMap<>();
+                newsItem.put("title", removeHTMLTags(title));
+                newsItem.put("link", link);
+                newsItem.put("description", removeHTMLTags(description));
+                newsList.add(newsItem);
+            }
+        } else {
+            log.error("No items found in the API response");
         }
         int totalResults = jsonNode.get("total").asInt(); // 뉴스의 전체 개수
 
         int itemsPerPage = 10; // 페이지당 뉴스 항목 수
         int startIndex = (page - 1) * itemsPerPage;
 
-        int newStartIndex = startIndex + 1; // start 값을 조정
+        int newStartIndex = start + 10 * (page - 1); // start 값을 조정
+
 
         int totalPages = (int) Math.ceil((double) totalResults / itemsPerPage);
 
+        // 네이버 검색 api는 start 파라미터를 최대 1000까지 받을 수 있기 때문에 전체 개수와 max 페이지 설정
+        if(totalResults >1000){
+            totalResults = 1000;
+            totalPages = 100;
+        }
+
         int endIndex = Math.min(startIndex + itemsPerPage, newsList.size());
 
-// 마지막 페이지에서의 endIndex 조정
+        // 마지막 페이지에서의 endIndex 조정
         if (page == totalPages) {
             endIndex = newsList.size() ;
         }
@@ -80,14 +91,17 @@ public String search(
 
         model.addAttribute("newsList", paginatedNewsList);
 
-
-
-        log.info(String.valueOf(totalPages));
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", totalPages);
         model.addAttribute("searchQuery", searchQuery);
 
         model.addAttribute("start", newStartIndex);
+
+        model.addAttribute("total", totalResults);
+
+
+        model.addAttribute("itemsPerPage", itemsPerPage);
+
     } catch (IOException e) {
         e.printStackTrace();
     }
@@ -95,6 +109,8 @@ public String search(
     log.info(searchResult);
     return "news/newsPage";
 }
+
+    // 받아온 api 데이터에서 html 태그들 제거 
     public static String removeHTMLTags(String input) {
 
         String cleanText = input
