74769efeba02d1d070393880ef7005a11c0ac133
wonderfulcho <wonderfulcho95@naver.com>
2023-08-30 22:48:17 +0900

    이게 진짜 수정본입니다

--- src/main/resources/static/js/apt/marker.js
+++ src/main/resources/static/js/apt/marker.js
@@ -58,108 +58,6 @@ function createMarker(position, markerContent, responseData) {
 
         // 마커 클릭시 오버레이 열리는 이벤트
         kakao.maps.event.addListener(marker, 'click', function () {
-<<<<<<< HEAD
-            closeOtherOverlays();
-            if (marker.overlay) {
-                if (marker.overlay.getMap()) {
-                    marker.overlay.setMap(null);
-                } else {
-                    marker.overlay.setMap(map);
-                }
-            }
-
-            // 사이드바 미니 지도 (staticMap)
-            var staticMapContainer = document.getElementById('staticMap');
-                staticMapContainer.innerHTML = ''; // 초기화 코드
-            var markerPosition = marker.getPosition();
-            var staticMapOption = {
-                center: markerPosition,
-                level: 4,
-                marker: marker
-            };
-            var staticMap = new kakao.maps.StaticMap(staticMapContainer, staticMapOption);
-
-            // 사이드바 정보 최신화
-            var title = responseData.complexName; // 아파트 이름
-            var roadName = responseData.roadName; // 도로명 주소
-            var address1 = responseData.district // 구주소
-            var address2 = responseData.address // 지번
-
-            // 사이드바 상단 타이틀
-            var asideBar = document.querySelector('.asideBarAll');
-            var titleElement = asideBar.querySelector('.title');
-
-            // 사이드바 내용 부분
-            var mapInfo = document.querySelector('.map-info');
-            var mapInfoTitleElement = mapInfo.querySelector('.title');
-            var mapInfoAddressElement = mapInfo.querySelector('.address');
-            var mapInfoRoadNameElement = mapInfo.querySelector('.roadName');
-
-            titleElement.textContent = title;
-
-            mapInfoTitleElement.textContent = title;
-            mapInfoAddressElement.textContent = address1 + " " + address2;
-            mapInfoRoadNameElement.textContent  = roadName;
-
-            $(".tbl tbody").empty();
-                        // 도로명 controller에 전달
-                        $.ajax({
-                            type: 'POST',
-                            url: '/map/main',
-                            data: {
-                                roadName: roadName
-                            },
-                            success: function (response) {
-                                var tableBody = $(".tbl tbody"); // 테이블의 tbody 요소 선택
-                                        if (response && response.aptRealTradeDTOList && response.aptRealTradeDTOList.length > 0) {
-                                            // 실거래정보 (연하늘색 부분)
-                                            var amount = response.aptRealTradeDTOList[0].transactionAmount.toString();
-                                            var amountSliceFirst = amount.slice(0, amount.length - 1);
-                                            var amountSliceLast = amount.slice(-1)
-                                            var floor = response.aptRealTradeDTOList[0].floor;
-                                            var txt = null;
-                                            if(amountSliceLast != 0) {
-                                                txt = amountSliceFirst + "억 " + amountSliceLast + "000만원(" + floor + "층)";
-                                            } else {
-                                                txt = amountSliceFirst + "억(" + floor + "층)";
-                                            };
-
-                                            var priceArea = document.querySelector(".price-area");
-                                            priceArea.querySelector(".txt").textContent = txt;
-
-
-                                            // 거래이력 테이블
-                                            for (var i = 0; i < response.aptRealTradeDTOList.length; i++) {
-                                                var detailItem = response.aptRealTradeDTOList[i];
-
-                                                // transactionAmount를 1억과 1억 미만의 금액으로 분리
-                                                var amount = detailItem.transactionAmount.toString();
-                                                var amountSliceFirst = amount.slice(0, amount.length - 1);
-                                                var amountSliceLast = amount.slice(-1)
-
-                                                // 테이블 행 생성 및 데이터 추가
-                                                var row = $("<tr>");
-                                                $("<td>").text(detailItem.area).appendTo(row);
-                                                $("<td>").text(detailItem.floor).appendTo(row);
-                                                $("<td>").text(detailItem.contractYearMonth).appendTo(row);
-                                               래
-
-                                                // 테이블에 행 추가
-                                                tableBody.append(row);
-                                            }
-                                } else {
-                                    console.log("표시할 aptDetailList 데이터가 없습니다.");
-                                }
-                            }
-                        });
-                    });
-
-
-                    clusterer.addMarker(marker); // 클러스터에 마커 추가
-                    existingMarkers[markerKey] = marker; // 생성된 마커를 객체에 추가
-                }
-            }
-=======
             closeOtherOverlays(); // 다른 오버레이 닫기
             openOverlay(marker.overlay); // 오버레이 열기
             updateSidebar(responseData); // 사이드바에 데이터 전송
@@ -170,7 +68,6 @@ function createMarker(position, markerContent, responseData) {
         existingMarkers[markerKey] = marker; // 생성된 마커를 마커리스트에 저장
     }
 }
->>>>>>> seoYoung
 
 // 한 번만 실행하기 위한 변수
 var onlyOneStart = false;
@@ -392,16 +289,11 @@ function closeOtherOverlays() {
 
 // keyword 입력 후 Enter 누르면 검색되는 함수
 function checkEnter(event) {
-<<<<<<< HEAD
-    if (event.key === 'Enter') {  // 엔터 키를 눌렀을 때만 작업을 수행
-        var keyword = document.querySelector('.aSearchInput').value;  // 입력 필드의 값 가져오기
-=======
     if (event.key === 'Enter') {
         // 입력한 키워드 공백 제거
         var keyword = document.querySelector('.aSearchInput').value.replaceAll(' ', '');
 
         closeOtherOverlays(); // 열려있는 오버레이 닫기
->>>>>>> seoYoung
 
         $.ajax({
             type: 'POST',
@@ -412,12 +304,6 @@ function checkEnter(event) {
             success: function (response) {
                 // 검색 결과에 따라 마커를 생성하고 지도에 표시하기
                 if (response && response.aptSearch) {
-<<<<<<< HEAD
-                var result = response.response.aptSearch;
-                console.log(result);
-                var markerPosition = new kakao.maps.LatLng(result.latitude, result.longitude);
-            }
-=======
                     var result = response.aptSearch; // 키워드 검색후 전송받은 해당 아파트 데이터
                     var newCenter = new kakao.maps.LatLng(result.latitude, result.longitude);
                     map.setCenter(newCenter); // 해당 아파트 위치로 센터 변경
@@ -440,7 +326,6 @@ function checkEnter(event) {
                         createMarker(markerPosition, markerContent, result);
                     }
                 }
->>>>>>> seoYoung
             }
         });
     }
