7d6e2982457ad2a59a145529b602316bc611f598
강준희 <>
2023-09-01 11:41:17 +0900

    상세정보창 토지정보 추가

--- src/main/resources/static/js/mapMain/mapMain.js
+++ src/main/resources/static/js/mapMain/mapMain.js
@@ -18,37 +18,35 @@
 //                });
 //    });
 
+
+
+
 const main = {
-    init : function() {
-        const _this = this;
+init : function() {
+const _this = this;
 
-        ...
-        
-        // 댓글 저장
-        $('#btn-comment-save').on('click', function () {
-            _this.commentSave();
-        });
+// 댓글 저장
+$('#btn-comment-save').on('click', function () {
+_this.commentSave();
+});
 
-    },
-    
-    ...
-    
-     /** 댓글 저장 */
-    commentSave : function () {
-        const data = {
-            postsId: $('#postsId').val(),
-            comment: $('#comment').val()
-        }
+},
+ /** 댓글 저장 */
+commentSave : function () {
+const data = {
+postsId: $('#postsId').val(),
+comment: $('#comment').val()
+}
 
-        // 공백 및 빈 문자열 체크
-        if (!data.comment || data.comment.trim() === "") {
-            alert("공백 또는 입력하지 않은 부분이 있습니다.");
-            return false;
-        } else {
-            $.ajax({
-                type: 'POST',
-                url: '/api/posts/' + data.postsId + '/comments',
-                dataType: 'JSON',
+// 공백 및 빈 문자열 체크
+if (!data.comment || data.comment.trim() === "") {
+alert("공백 또는 입력하지 않은 부분이 있습니다.");
+return false;
+} else {
+$.ajax({
+type: 'POST',
+url: '/api/posts/' + data.postsId + '/comments',
+dataType: 'JSON',
                 contentType: 'application/json; charset=utf-8',
                 data: JSON.stringify(data)
             }).done(function () {
