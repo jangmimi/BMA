2a69070ad81768369ef2d930e4ef2dc78e064d41
Kimgimu <vouncr88@gmail.com>
2023-09-04 03:08:35 +0900

    .

---
+++ src/main/java/com/ap4j/bma/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.ap4j.bma.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import javax.servlet.http.HttpSession;
+@Slf4j
+@Controller
+@SessionAttributes("loginMember")
+public class HomeController {
+
+    @RequestMapping("/")
+    public String mainPage(HttpSession session){
+        log.info("session userEmail : " + session.getAttribute("loginMember"));
+        return "mainPage/mainPage";
+    }
+
+}
