2cb7e213474cd3e88a5231081ace6835c15b7405
noeyhijnuY <yun0217@kakao.com>
2023-09-04 17:06:06 +0900

    올려 올려 에러남

--- src/main/resources/static/js/maemulReg/maemulinfo.js
+++ src/main/resources/static/js/maemulReg/maemulinfo.js
@@ -52,7 +52,7 @@
         }
 
      // 카카오 도로명 주소 검색 팝업을 열기 위한 함수
-       function openKakaoAddressSearch() {
+       function openKakaoAddressSearch(event) {
            new daum.Postcode({
                oncomplete: function(data) {
                    // 선택한 주소 정보를 입력 필드에 채우는 작업을 수행하세요.
@@ -61,40 +61,43 @@
            }).open();
        }
 
-         // 내 위치 정보 가져오기 함수
-            function getCurrentLocation() {
-                if ("geolocation" in navigator) {
-                    navigator.geolocation.getCurrentPosition(function(position) {
-                        // 현재 위치 정보를 처리하는 작업을 수행하세요.
-                        var latitude = position.coords.latitude;
-                        var longitude = position.coords.longitude;
 
-                        // 위도와 경도를 주소로 변환하는 함수 호출
-                        convertLatLongToAddress(latitude, longitude);
-                    });
-                } else {
-                    alert("브라우저가 위치 정보를 지원하지 않습니다.");
-                }
-            }
+//내위치 기능
 
-            // 위도와 경도를 주소로 변환하는 함수
-            function convertLatLongToAddress(latitude, longitude) {
-                var geocoder = new google.maps.Geocoder();
-                var latlng = new google.maps.LatLng(latitude, longitude);
-
-                geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        if (results[0]) {
-                            var formattedAddress = results[0].formatted_address;
-                            document.getElementById('addressInput').value = formattedAddress;
-                        } else {
-                            alert('주소를 찾을 수 없습니다.');
-                        }
-                    } else {
-                        alert('Geocoder에 실패했습니다: ' + status);
-                    }
-                });
-            }
+//         // 내 위치 정보 가져오기 함수
+//            function getCurrentLocation() {
+//                if ("geolocation" in navigator) {
+//                    navigator.geolocation.getCurrentPosition(function(position) {
+//                        // 현재 위치 정보를 처리하는 작업을 수행하세요.
+//                        var latitude = position.coords.latitude;
+//                        var longitude = position.coords.longitude;
+//
+//                        // 위도와 경도를 주소로 변환하는 함수 호출
+//                        convertLatLongToAddress(latitude, longitude);
+//                    });
+//                } else {
+//                    alert("브라우저가 위치 정보를 지원하지 않습니다.");
+//                }
+//            }
+//
+//            // 위도와 경도를 주소로 변환하는 함수
+//            function convertLatLongToAddress(latitude, longitude) {
+//                var geocoder = new google.maps.Geocoder();
+//                var latlng = new google.maps.LatLng(latitude, longitude);
+//
+//                geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+//                    if (status == google.maps.GeocoderStatus.OK) {
+//                        if (results[0]) {
+//                            var formattedAddress = results[0].formatted_address;
+//                            document.getElementById('addressInput').value = formattedAddress;
+//                        } else {
+//                            alert('주소를 찾을 수 없습니다.');
+//                        }
+//                    } else {
+//                        alert('Geocoder에 실패했습니다: ' + status);
+//                    }
+//                });
+//            }
 
         //팝업
     function showPopup(event) {
