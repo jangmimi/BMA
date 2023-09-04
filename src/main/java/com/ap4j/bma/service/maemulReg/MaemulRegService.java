20f6a59622b033922e530129d855c9afe8657b6d
noeyhijnuY <yun0217@kakao.com>
2023-09-05 02:41:49 +0900

    잠들까봐 올려놓기

--- src/main/java/com/ap4j/bma/service/maemulReg/MaemulRegService.java
+++ src/main/java/com/ap4j/bma/service/maemulReg/MaemulRegService.java
@@ -5,14 +5,28 @@ import com.ap4j.bma.model.repository.MaemulRegRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class MaemulRegService {
 
-    @Autowired
-    private MaemulRegRepository maemulRegRepository;
+    private final MaemulRegRepository maemulRegRepository;
 
+    @Autowired
+    public MaemulRegService(MaemulRegRepository maemulRegRepository) {
+        this.maemulRegRepository = maemulRegRepository;
+    }
+
+    // 매물 정보 저장
+    @Transactional
     public MaemulRegEntity saveMaemulInfo(MaemulRegEntity maemulRegEntity) {
-        // 매물 정보를 데이터베이스에 저장
         return maemulRegRepository.save(maemulRegEntity);
+    }
+
+    public MaemulRegEntity getMaemulById(Integer maemulId) {
+        // 매물 ID로 매물 정보를 조회
+        return maemulRegRepository.findById(maemulId).orElse(null);
     }
 }
