1e89324ca66ad5163d0d885b2dec3a433538cf8f
wonderfulcho <wonderfulcho95@naver.com>
2023-09-07 01:18:10 +0900

    maemulRegEntity, DTO, service, confirmation.html, maemulinfo.html 수정

--- src/main/java/com/ap4j/bma/model/entity/meamulReg/MaemulRegEntity.java
+++ src/main/java/com/ap4j/bma/model/entity/meamulReg/MaemulRegEntity.java
@@ -1,6 +1,8 @@
 package com.ap4j.bma.model.entity.meamulReg;
 
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 import javax.persistence.*;
 
@@ -11,6 +13,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@DynamicInsert
 public class MaemulRegEntity {
 
     @Id
@@ -23,37 +26,51 @@ public class MaemulRegEntity {
     private String APT_name; //아파트 이름
 
     private String buildingUsage; //건축물 용도
-    private int numberOfRooms; //방 수
-    private int numberOfBathrooms; //욕실 수
-    private int floorNumber; //해당층
-    private int totalFloors; //건물층
+
+    private Integer numberOfRooms; //방 수
+
+    private Integer numberOfBathrooms; //욕실 수
+
+    private Integer floorNumber; //해당층
+
+    private Integer totalFloors; //건물층
     private Double privateArea; //전용면적
     private Double supplyArea; //공급면적
     private String direction; //방향
     private String heatingType;//난방종류
-    private String Elevator;//엘리베이터(value를 1,0으로 받아오면 int값으로 저장될 거 같아서 int로함)
+    private String Elevator;//엘리베이터
     private String Parking;//주차가능여부 (엘베랑 같은 이유)
-    private int totalParking;//총 주차대수
+
+
+    private Integer totalParking;//총 주차대수
     private String shortTermRental;//단기임대
     private String availableMoveInDate;//입주가능일
-    private Long loanAmount;//융자금
+
+    @ColumnDefault("0")
+    private Integer loanAmount;//융자금
 
 
     //거래 유형
     private String tradeType;//거래 유형
 
     //월세
-    private Long monthlyForRent; //희망보증금
-    private Long monthlyRent; //희망월세
-    private Long m_managementFee;//관리비
+    @ColumnDefault("999")
+    private Integer monthlyForRent; //보증금
+    @ColumnDefault("999")
+    private Integer monthlyRent; //월세
 
 
     //전세
-    private Long depositForLease; //희망 전세
-    private Long d_managementFee; //관리비
+    @ColumnDefault("999")
+    private Integer depositForLease; //희망 전세
+
+    @ColumnDefault("999")
+    private Integer managementFee;//전 월세 관
 
     //매매
-    private Long SellingPrice;//희망매매가
+    @Column(columnDefinition = "INT")
+    @ColumnDefault("'999'")
+    private Integer SellingPrice;//희망매매가
 
     //상세정보 페이지
     private String title; //제목
@@ -69,5 +86,8 @@ public class MaemulRegEntity {
     //좌표
     private Double longitude; //경도
     private Double latitude; //위도
+
+    //등록일자
+    private String createdAt;
 
 }
