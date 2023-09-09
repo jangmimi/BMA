d6d86d33ecd6f7b7333086c37f55a6bf869677e9
jangmimi <alwkd0101@naver.com>
2023-09-09 22:30:07 +0900

    [마이페이지]관심,나의매물 조회로 탭 조회 구현 완료

--- src/main/java/com/ap4j/bma/controller/member/MemberController.java
+++ src/main/java/com/ap4j/bma/controller/member/MemberController.java
@@ -241,6 +241,11 @@ public class MemberController {
         Page<MaemulRegEntity> mmpList = likedService.getPaginatedItems(nickname,page,pageSize);
         Long likedCnt = likedService.countLikedByNickname(nickname);
 
+        /* 매물목록으로 테스트중*/
+        List<MaemulRegEntity> mRecentList = qMemberService.getListByNickname(loginMember.getNickname());
+        model.addAttribute("mRecentList",mRecentList);
+        /**/
+
         model.addAttribute("root", root);
         model.addAttribute("thumbnail_image", thumImg);
         model.addAttribute("likedCnt",likedCnt);
