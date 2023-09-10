e36f25f253d144c484042d674df6a8172875e014
우진 <leewoojin982@gmail.com>
2023-09-11 00:59:52 +0900

    Feat(miniHome.html): 거주용 업무용 버튼 활성화. Feat(details.html): 주변 상권 커스텀 활성화

--- src/main/resources/static/js/miniHome/miniHome.js
+++ src/main/resources/static/js/miniHome/miniHome.js
@@ -1,31 +1,42 @@
 $(()=>{
 	let 거주용 = $('.z-top-seperate1');
 	let 업무용 = $('.z-top-seperate2');
-	$('.z-top-seperate2 .z-count').css('color', '#EFEFF0');
+	let 거주용_카운트 = $('.z-top-seperate1 .z-count');
+	let 업무용_카운트 = $('.z-top-seperate2 .z-count');
+	let 거주용_매물 = $('.z-home');
+	let 업무용_매물 = $('.z-work');
+
+	업무용_카운트.css('color', '#888');
 
 	거주용.css('border-bottom', '2px solid #000'); // 거주용 아래 밑줄
 	업무용.css('border-bottom', 'none'); // 업무용 아래 밑줄 없앰
+	거주용_매물.css('display', 'block');
+	업무용_매물.css('display', 'none');
 
 	var is업무용Clicked = false;
 	var is거주용Clicked = false;
-	console.log("시작됨");
 
     // 거주용 버튼 클릭 시
 	거주용.click(()=>{
 		if(!is거주용Clicked){
 			업무용.css('border-bottom', 'none');
+			업무용_카운트.css('color', '#888');
 			거주용.css('border-bottom', '2px solid #000');
-
+			거주용_카운트.css('color', '#129ded');
+			거주용_매물.css('display', 'block');
+			업무용_매물.css('display', 'none');
 		}
-
 	})
 
 	// 업무용 버튼 클릭 시
 	업무용.click(()=>{
 		if(!is업무용Clicked){
 			거주용.css('border-bottom', 'none');
+			거주용_카운트.css('color', '#888');
 			업무용.css('border-bottom', '2px solid #000');
+			업무용_카운트.css('color', '#129ded');
+			거주용_매물.css('display', 'none');
+			업무용_매물.css('display', 'block');
 		}
-
 	})
 })
